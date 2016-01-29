eac9692437b17433483f19dd5b181cb0b253d59a
lolgans <t.katzelol@gmail.com>
2016-01-29 14:12:47 +0100

    tsneExample

--- TSNE example.py
+++ TSNE example.py
@@ -8,6 +8,7 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 # load Word2Vec model
 model = gensim.models.Word2Vec.load('models/5k_MinCount2')
 
+# set vecs and word labels
 word_labels = ['1.is', '2.is', '3.is', '4.is', '5.is']
 vecs = []
 vecs.append(model['is'].reshape(1, 100))
@@ -15,16 +16,15 @@ vecs.append(model['is'].reshape(1, 100))
 vecs.append(model['is'].reshape(1, 100))
 vecs.append(model['is'].reshape(1, 100))
 vecs.append(model['is'].reshape(1, 100))
-
 vecs = np.concatenate(vecs)
 vecs = np.array(vecs, dtype='float')
 
+# initalize TSNE
 ts = TSNE(2)
 reduced_vecs = ts.fit_transform(vecs)
 
-fig = plt.figure()
-
 #  plot it
+fig = plt.figure()
 for i in range(len(reduced_vecs)):
     color = 'g'
     plt.plot(reduced_vecs[i, 0], reduced_vecs[i, 1], marker='o', color=color, markersize=8)
